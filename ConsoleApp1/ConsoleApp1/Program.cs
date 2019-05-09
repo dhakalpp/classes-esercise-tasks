e16e313b377d0349b286743927b1999c820e15e3
Priyanka Dhakal <30001935@student.toiohomai.ac.nz>
2019-05-09 13:30:34 +1200

    classes exercise task 2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Task2*/
+            priyanka Priyanka = new priyanka("Priyanka", "Dhakal", 21);
+            Console.WriteLine("person's details");
+            Console.WriteLine($"Name: {Priyanka.Fname} {Priyanka.Lname}\nAge: {Priyanka.Age}");
+            string fname, lname;
+            int age;
+            fname = Console.ReadLine();
+            lname = Console.ReadLine();
+            age = int.Parse(Console.ReadLine());
+            priyanka priyanka = new priyanka(fname, lname, age);
+
+
+
+        }
+        
+        
+           
+
+            
+        
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/priyanka.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class priyanka
+    {
+        /*Attributes*/
+        string fname;
+        string lname;
+        int age;
+        /*Attributes getters ans setter (combined make properties)*/
+        public string Fname { get { return fname; } set { fname = value; } }
+        public string Lname { get { return lname; } set { lname = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        /*constructer*/
+        public priyanka(string _fname, string _lname, int _age)
+        {
+            Fname = _fname;
+            Lname = _lname;
+            Age = _age;
+        }
+    }
+}
